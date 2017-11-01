7a28e66e52763bf562fef18c10e07321bb72cbce
Tech-A20 <Tech-A20@Tech-A20s-MacBook-Air.local>
2017-11-01 11:00:26 +0000

    get user input

---
+++ solution/main.js
@@ -0,0 +1,21 @@
+//get the player input
+function getUserInput() {
+  return prompt ("please chose either 'rock', 'paper' or 'scissors'.")
+}
+var test = getUserInput();
+
+console.log(test);
+
+//get the plyer move
+
+
+//random player
+
+
+//get the computer move
+
+
+//will need to play to five
+
+
+//check for a winner
