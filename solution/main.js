603268e4c760570191d45c2eeeca1cafa4af9105
Tech-A20 <Tech-A20@Tech-A20s-MacBook-Air.local>
2017-11-01 11:56:59 +0000

    work in progress

--- solution/main.js
+++ solution/main.js
@@ -1,33 +1,66 @@
-//get the player input
+// Get input
 function getUserInput() {
-  return prompt ("please chose either 'rock', 'paper' or 'scissors'.")
+  return prompt("Please choose either 'rock', 'paper', or 'scissors'.");
 }
 
-//get the plyer move
-function getPlayerMove() {
-  return getUserInput();
-}
-
-//random player
+// Random play
 function randomPlay() {
-  // var randomNumber = Math.floor(Math.random() * 3)
-  var randomNumber = 0;
-if (randomNumber === 0) {
-  return "rock"
-} else if (randomNumber === 1) {
-  return"paper"
-}else{
-  return "scissors"
+  var randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+    return "rock";
+  } else if (randomNumber === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+// get the player move
+function getPlayerMove(playerMove) {
+  return playerMove || getUserInput();
+}
+
+// get the computer move
+function getComputerMove(computerMove) {
+  return computerMove || randomPlay();
+}
+
+// var test = getComputerMove();
+// console.log("This is the computer move --- ", test);
+
+// will need to play to five
+function startGame() {
+  alert("Lets player Rock, Paper, Scissors");
+  // score variables
+  var playerScore = 0;
+  var computerScore = 0;
+
+  // play to five
+  while(playerScore < 5 && computerScore < 5) {
+    var playerMove = getPlayerMove();
+    var computerMove = getComputerMove();
+    // console.log("PlayerMove --- ", playerMove);
+    // console.log("ComputerMove --- ", computerMove);
+    playerScore = 6;
+    computerScore = 6;
+
+
+
   }
 
-
 }
 
+// check for a winner
+function checkForWinner(playerMove, computerMove) {
+  var winner;
 
-//get the computer move
+  if(playerMove === computerMove){
+    winner = "tie";
+    return winner;
+  }
+}
 
+var test = checkForWinner("rock", "rock");
+console.log(test)
 
-//will need to play to five
-
-
-//check for a winner
+startGame()
