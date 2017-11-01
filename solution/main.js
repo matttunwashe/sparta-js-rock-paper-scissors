8776133538f03048d3fad429f4e22a107b8ffa3e
Tech-A20 <Tech-A20@Tech-A20s-MacBook-Air.local>
2017-11-01 11:20:59 +0000

    got the random play working

--- solution/main.js
+++ solution/main.js
@@ -2,14 +2,26 @@
 function getUserInput() {
   return prompt ("please chose either 'rock', 'paper' or 'scissors'.")
 }
-var test = getUserInput();
-
-console.log(test);
 
 //get the plyer move
-
+function getPlayerMove() {
+  return getUserInput();
+}
 
 //random player
+function randomPlay() {
+  // var randomNumber = Math.floor(Math.random() * 3)
+  var randomNumber = 0;
+if (randomNumber === 0) {
+  return "rock"
+} else if (randomNumber === 1) {
+  return"paper"
+}else{
+  return "scissors"
+  }
+
+
+}
 
 
 //get the computer move
